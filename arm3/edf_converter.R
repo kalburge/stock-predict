2fb324c708ad1b802938622ba3e221ae8e769986
Ishan Kalburge <33986737+kalburge@users.noreply.github.com>
2023-06-16 15:45:38 -0400

    converter up-to-date

--- arm3/edf_converter.R
+++ arm3/edf_converter.R
@@ -2,7 +2,7 @@
 library(eyelinkReader)
 
 results_path <- "/Users/goldlab/Documents/Ishan_summer23/test_runs/arm3/results/"
-run_path <- "2023_06_01_14_15"
+run_path <- "2023_06_01_16_07"
 filename <- "ARM3_ET.EDF"
 setwd(paste(results_path,run_path,sep=""))
 gaze <- read_edf(filename, import_samples=TRUE)
