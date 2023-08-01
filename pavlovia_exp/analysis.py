798211166f289db87b08d79ab6ab274072ea7d73
Ishan Kalburge <33986737+kalburge@users.noreply.github.com>
2023-08-01 17:30:57 -0400

    analysis updates

--- pavlovia_exp/analysis.py
+++ pavlovia_exp/analysis.py
@@ -21,7 +21,8 @@ def get_files_in_directory(directory):
         # checking if it is a file
         if os.path.isfile(f) and f[-4:] == ".csv":
             files.append(f)
-    return files
+    print("There are " + str(len(files)) + " subject's data files in this folder.")
+    return files, len(files)
 
 def load(filepath, thresh, showBonus=False):
     df = pd.read_csv(filepath)
@@ -92,7 +93,7 @@ def convert_to_bursty(choice, idx):
 def block(n, df, start):
     k = int(df.shape[0]/numBlocks)
     block = df.iloc[(n-1)*k: n*k]
-    return block.iloc[start:]
+    return block.iloc[start: -5]
 
 def accuracy(df):
     commits = df[df['commit'] == 1]
@@ -111,6 +112,15 @@ def compute_bonus(df, thresh):
         return round(final_score/bonus_threshold,2)
     else:
         return 0
+
+def get_score(df, thresh):
+    dp_threshold = np.sum(thresh)
+    avg_score_per_block = [sc - round(sc/2, 0) for sc in thresh]
+    bonus_threshold = np.sum(avg_score_per_block)
+    final_score = df['score'].iloc[-1]
+    # if final_score >= dp_threshold:
+    #     return "Exceeded DP algorithm score"
+    return final_score
 
 def compute_burstiness(lags_line):
     mean = np.mean(lags_line)
@@ -171,7 +181,7 @@ def get_linear_fit(x,y):
     inp = np.linspace(np.min(x), np.max(x), 8)
     outp = slope*inp + intercept
     inp = inp.reshape(-1,1); outp = outp.reshape(-1,1)
-    return inp, outp, linestyle, color
+    return inp, outp, slope, intercept, p, linestyle, color
 
 def get_sin_fit(x,y):
     dataset = np.concatenate((x[:,0].reshape(-1,1), y[:,0].reshape(-1,1)), axis=1)
@@ -232,78 +242,96 @@ def get_abs_threshold(data, d=0):
     
     return q, eps, threshold_price
     
+def show_bonuses(files, eps_thresh):
+    print("The following participants received a bonus. Results are displayed in the format id,bonus:\n")
+    for f in files:
+        pid, df, acc, length = load(f, eps_thresh, True)
+
+
+def get_population_summary(files, eps_thresh):
+    id = []; accuracy = []; time_taken = []; score = []; rec_bonus = []; slope = []; intercept = []; pval = []
+    change_in_eps = []; num_commits = []
+    discard = 70
+    for f in files:
+        pid, df, acc, length = load(f, eps_thresh)
+        s, i, p = get_iai_eps_slopes(df, discard)
+        id.append(pid); accuracy.append(acc); time_taken.append(length); 
+        score.append(get_score(df,eps_thresh)); rec_bonus.append((compute_bonus(df, eps_thresh) != 0))
+        slope.append(s); intercept.append(i); pval.append(p)
+        change_in_eps.append(get_change_iai(df,discard))
+        num_commits.append(len(df[df['commit'] == 1]))
+    
+    id = np.array(id); id = id.reshape(-1,1)
+    accuracy = np.array(accuracy); accuracy = accuracy.reshape(-1,1)
+    time_taken = np.array(time_taken); time_taken = time_taken.reshape(-1,1)
+    score = np.array(score); score = score.reshape(-1,1)
+    rec_bonus = np.array(rec_bonus); rec_bonus = rec_bonus.reshape(-1,1)
+    slope = np.array(slope); slope = slope.reshape(-1,1)
+    intercept = np.array(intercept); intercept = intercept.reshape(-1,1)
+    pval = np.array(pval); pval = pval.reshape(-1,1)
+    change_in_eps = np.array(change_in_eps); change_in_eps = change_in_eps.reshape(-1,1)
+    num_commits = np.array(num_commits); num_commits = num_commits.reshape(-1,1)
+    mat = np.concatenate((id, accuracy, time_taken, score, 
+            rec_bonus, slope, intercept, pval, change_in_eps, num_commits), axis=1)
+
+    show_summary_plots(accuracy, time_taken, score, rec_bonus, slope, intercept, pval, change_in_eps, num_commits)
+    return pd.DataFrame(mat, columns=['id', 'accuracy', 'time_taken', 'score', 'received_bonus', 'slope', 'intercept', 'pval', 'change_in_eps', 'num_commits'])
+
+
+def show_summary_plots(accuracy, time_taken, score, rec_bonus, slope, intercept, pval, change_in_eps, num_commits):
+    fig, ax = plt.subplots(figsize=(9,12),dpi=150)
+    plt.subplots_adjust(wspace=0.35, hspace = 0.3) 
+    plt.subplot(321)
+    plt.scatter(accuracy, score, c=rec_bonus)
+    plt.hlines(y=492, xmin = min(accuracy), xmax = max(accuracy), colors = ['black'], linestyles = 'dashed')
+    plt.xlabel("Accuracy")
+    plt.ylabel("Score")
+    plt.title("Subject score vs. accuracy", fontsize = 10)
+    plt.subplot(322)
+    plt.scatter(accuracy, num_commits, c=score)
+    plt.xlabel("Accuracy")
+    plt.ylabel("Number of commits")
+    plt.title("Subject number of commits (s) vs. accuracy (color = score)", fontsize = 10)
+    plt.colorbar()
+    plt.subplot(323)
+    plt.scatter(accuracy, time_taken, c=score)
+    plt.xlabel("Accuracy")
+    plt.ylabel("Time Taken (s)")
+    plt.title("Subject time taken (s) vs. accuracy (color = score)", fontsize = 10)
+    plt.colorbar()
+    plt.subplot(324)
+    plt.scatter(accuracy, slope, c=score)
+    plt.xlabel("Accuracy")
+    plt.ylabel("Slope")
+    plt.title("Subject slope (s) vs. accuracy (color = score)", fontsize = 10)
+    plt.colorbar()
+    plt.subplot(325)
+    plt.scatter(accuracy, intercept, c=score)
+    plt.xlabel("Accuracy")
+    plt.ylabel("Intercept")
+    plt.title("Subject intercept (s) vs. accuracy (color = score)", fontsize = 10)
+    plt.colorbar()
+    plt.subplot(326)
+    plt.scatter(accuracy, change_in_eps, c=score)
+    plt.xlabel("Accuracy")
+    plt.ylabel("Change in $\mu_\\tau$ across $\epsilon$")
+    plt.title("Subject intercept (s) vs. change in $\mu_\\tau$ across $\epsilon$ (color = score)", fontsize = 10)
+    plt.colorbar()
+    # plt.subplot(326)
+    # plt.scatter(accuracy, intercept, c=score)
+    # plt.xlabel("Accuracy")
+    # plt.ylabel("Intercept")
+    # plt.title("Subject intercept (s) vs. accuracy (color = score)", fontsize = 11)
+    # plt.colorbar()
 
     
-def plot_eps(data, d=0):
-    eps = []; b_i = []; mu_i = []; std_i = [];
-    fig, ax = plt.subplots(figsize=(7,2.5),dpi=150)
-    plt.subplots_adjust(wspace=0.4)
-    for i in range(1,numBlocks+1):
-        curBlock = block(i,data,d)
-        all_lags, lags, freq, N = convert_to_bursty(np.array(curBlock.commit), 1)
-        eps.append(curBlock['eps_prob'].iloc[1])
-        b_i.append(compute_burstiness(all_lags))
-        mu_i.append(compute_mean(all_lags))
-        std_i.append(compute_std(all_lags))
-    
-    eps = np.array(eps); eps = eps.reshape(-1,1)
-    b_i = np.array(b_i); b_i = b_i.reshape(-1,1)
-    mu_i = np.array(mu_i); mu_i = mu_i.reshape(-1,1)
-    std_i = np.array(std_i); std_i = std_i.reshape(-1,1)
-    
-    plt.subplot(131)
-    plt.scatter(eps,b_i)
-    plt.xlabel('$\epsilon$'); plt.ylabel('$B$')
-    eps_mdl = LinearRegression(); eps_mdl.fit(eps,b_i)
-    plt.plot(eps, eps_mdl.predict(eps), 'r')
-    
-    plt.subplot(132)
-    plt.scatter(eps,mu_i)
-    plt.xlabel('$\epsilon$'); plt.ylabel('$\mu$')
-    eps_mdl = LinearRegression(); eps_mdl.fit(eps,mu_i)
-    plt.plot(eps, eps_mdl.predict(eps), 'r')
-    
-    plt.subplot(133)
-    plt.scatter(eps,std_i)
-    plt.xlabel('$\epsilon$'); plt.ylabel('$\sigma$')
-    eps_mdl = LinearRegression(); eps_mdl.fit(eps,std_i)
-    plt.plot(eps, eps_mdl.predict(eps), 'r')
-    plt.show()
-    
-    
-    
-def plot_q(data):
-    q = []; b_i = []; mu_i = []; std_i = [];
-    fig, ax = plt.subplots(figsize=(7,2.5),dpi=150)
-    plt.subplots_adjust(wspace=0.4)
-    for i in range(1,numBlocks+1):
-        curBlock = block(i,data)
-        all_lags, lags, freq, N = convert_to_bursty(np.array(curBlock.commit), 1)
-        q.append(curBlock['q_prob'].iloc[1])
-        b_i.append(compute_burstiness(all_lags))
-        mu_i.append(compute_mean(all_lags))
-        std_i.append(compute_std(all_lags))
-    
-    q = np.array(q); q = q.reshape(-1,1)
-    b_i = np.array(b_i); b_i = b_i.reshape(-1,1)
-    mu_i = np.array(mu_i); mu_i = mu_i.reshape(-1,1)
-    std_i = np.array(std_i); std_i = std_i.reshape(-1,1)
-    
-    plt.subplot(131)
-    plt.scatter(q,b_i)
-    plt.xlabel('$q$'); plt.ylabel('$B$')
-    q_mdl = LinearRegression(); q_mdl.fit(q,b_i)
-    plt.plot(q, q_mdl.predict(q), 'r')
-    
-    plt.subplot(132)
-    plt.scatter(q,mu_i)
-    plt.xlabel('$q$'); plt.ylabel('$\mu$')
-    q_mdl = LinearRegression(); q_mdl.fit(q,mu_i)
-    plt.plot(q, q_mdl.predict(q), 'r')
-    
-    plt.subplot(133)
-    plt.scatter(q,std_i)
-    plt.xlabel('$q$'); plt.ylabel('$\sigma$')
-    q_mdl = LinearRegression(); q_mdl.fit(q,std_i)
-    plt.plot(q, q_mdl.predict(q), 'r')
-    plt.show()
+
+def get_iai_eps_slopes(df, discard):
+    __, eps, mu_i, __ = get_mean_iai(df, discard)
+    x, y, s, i, p, l, c = get_linear_fit(eps,mu_i)
+    # plt.plot(x,y, linestyle = l, c=c)
+    return s, i, p
+
+def get_change_iai(df, discard):
+    __, eps, mu_i, __ = get_mean_iai(df, discard)
+    return mu_i[-1] - mu_i[0]
